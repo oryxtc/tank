041eb1aa7ff71bb3f0bab5ee9ac6fc4c07f00fb9
oryxtc <oryxtc@gmail.com>
2018-12-02 11:33:37 +0800

    拆解

--- app/Http/Controllers/Tank/PlayerController.php
+++ app/Http/Controllers/Tank/PlayerController.php
@@ -10,23 +10,31 @@ namespace App\Http\Controllers\Tank;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Models\TankModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class PlayerController extends Controller
 {
-    const TEAM_NAME = 'tB';
-    private $team = null;
+    private $team;
+    private $enemyTeam;
+    private $teamId;
+    private $enemyTeamId;
+    private $mapRowLen;
+    private $mapColLen;
+
+    private $newMap = [];
+
 
     public function __construct()
     {
-        $request = Request()->all();
-        Log::info($request);
-        if ($request['team'] === self::TEAM_NAME) {
-            $this->team = 'B';
-        } else {
-            $this->team = 'C';
-        }
+        $request           = Request()->all();
+        $this->team        = $request['team'];
+        $this->enemyTeam   = $this->team === 'tB' ? 'tC' : 'tB';
+        $this->teamId      = substr($this->team, -1);
+        $this->enemyTeamId = $this->teamId === 'B' ? 'C' : 'B';
+        $this->mapRowLen   = $request['view']['rowLen'];
+        $this->mapColLen   = $request['view']['colLen'];
     }
 
     public function init(Request $request)
@@ -37,34 +45,64 @@ class PlayerController extends Controller
 
     public function action(Request $request)
     {
-        $input        = $request->all();
+        $requestData = $request->all();
+        $mapData     = $requestData['view']['map'];
+
+        foreach ($mapData as $rowKey => $rowData) {
+            $this->newMap[$rowKey] = $rowData;
+            foreach ($rowData as $colKey => $val) {
+                $this->newMap[$rowKey][$colKey] = [];
+                if (!isset($this->newMap[$rowKey][$colKey]['weights'])) {
+                    $this->newMap[$rowKey][$colKey]['weights'] = 0;
+                }
+                //TODO 计算坦克对上下左右权重影响
+                //                switch ($val){
+                //                    case 'M1'
+                //                }
+                $this->newMap[$rowKey][$colKey]['weights'] += TankModel::$elementWeights[$val];
+            }
+        }
+
+        $teamTanks = $requestData[$this->team];
+        foreach ($teamTanks as $tankKey => $tank) {
+            $lengthMax = $tank['yidong'];
+            $rangeMax  = $tank['shecheng'];
+            $viewMax   = $tank['shiye'];
+            //赋值返回值
+            $responseData[$tankKey] = [
+                'tId' => $tank['tId']];
+            //计算该坦克上下左右权重
+
+        }
+        Log::info($this->newMap);
+
         $responseData = [
             [
-                'tId'       => "{$this->team}1",
+                'tId'       => "{$this->teamId}1",
                 'direction' => 'DOWN',
                 'type'      => 'MOVE',
                 'length'    => 1,
                 'useGlod'   => true],
             [
-                'tId'       => "{$this->team}2",
+                'tId'       => "{$this->teamId}2",
                 'direction' => 'DOWN',
                 'type'      => 'FIRE',
                 'length'    => 1,
                 'useGlod'   => false],
             [
-                'tId'       => "{$this->team}3",
+                'tId'       => "{$this->teamId}3",
                 'direction' => 'DOWN',
                 'type'      => 'FIRE',
                 'length'    => 1,
                 'useGlod'   => false],
             [
-                'tId'       => "{$this->team}4",
+                'tId'       => "{$this->teamId}4",
                 'direction' => 'DOWN',
                 'type'      => 'FIRE',
                 'length'    => 1,
                 'useGlod'   => false],
             [
-                'tId'       => "{$this->team}5",
+                'tId'       => "{$this->teamId}5",
                 'direction' => 'DOWN',
                 'type'      => 'FIRE',
                 'length'    => 1,
---
+++ app/Http/Models/TankModel.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oryxtc
+ * Date: 2018/12/2
+ * Time: 10:46
+ */
+
+namespace App\Http\Models;
+
+
+class TankModel
+{
+    public static $elementWeights=[
+        'M1'=>0,
+        'M2'=>100,
+        'M3'=>10,
+        'M4'=>-10,
+        'M5'=>-10,
+        'M6'=>-10,
+        'M7'=>-10,
+        'M8'=>-10,
+        'A1'=>20,
+        'B1'=>20,
+        'B2'=>20,
+        'B3'=>10,
+        'B4'=>10,
+        'B5'=>10,
+        'C1'=>20,
+        'C2'=>20,
+        'C3'=>10,
+        'C4'=>10,
+        'C5'=>10,
+    ];
+}
