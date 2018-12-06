47c61adebf80bcc01eee7f949ac4aa2e7b25be44
oryxtc <oryxtc@gmail.com>
2018-12-06 23:13:35 +0800

    偏攻击性

--- app/Http/Controllers/Tank/PlayerController.php
+++ app/Http/Controllers/Tank/PlayerController.php
@@ -24,6 +24,7 @@ class PlayerController extends Controller
     public $mapRowLen;
     public $mapColLen;
     public $boos;
+    public $request;
 
     public $newMap = [];
 
@@ -31,7 +32,8 @@ class PlayerController extends Controller
     public function __construct()
     {
         $request           = Request()->all();
-        $this->boos=    $request['tA'];
+        $this->request     = $request;
+        $this->boos        = $request['tA'];
         $this->team        = $request['team'];
         $this->enemyTeam   = $this->team === 'tB' ? 'tC' : 'tB';
         $this->teamId      = substr($this->team, -1);
@@ -43,8 +45,7 @@ class PlayerController extends Controller
     public function init(Request $request)
     {
         $input = $request->all();
-//        Cache::forget('start_date');
-        Cache::put('start_date',time(),20);
+        Cache::put('start_date', time(), 20);
         return $this->apiReturn(null, "/init");
     }
 
@@ -71,8 +72,8 @@ class PlayerController extends Controller
         $teamTanks = $requestData[$this->team]['tanks'];
         $teamTanks = array_column($teamTanks, null, 'tId');
         //计算路线&射击
-        $newMap = $this->newMap;
-        $responseData=[];
+        $newMap       = $this->newMap;
+        $responseData = [];
         foreach ($newMap as $newRowKey => $newRoWData) {
             foreach ($newRoWData as $newColKey => $newItem) {
                 if (preg_match("/{$this->teamId}\d/", $newItem['element'])) {
@@ -81,7 +82,7 @@ class PlayerController extends Controller
                     //优先攻击
                     $noteData = (new TankModel())->computeAttack(array_merge($tankData, $newItem), $newRowKey, $newColKey, $newMap);
                     if (empty($noteData)) {
-                        $noteData = (new TankModel())->computeRandom(array_merge($tankData, $newItem), $newRowKey, $newColKey, $newMap,true);
+                        $noteData = (new TankModel())->computeRandom(array_merge($tankData, $newItem), $newRowKey, $newColKey, $newMap, true);
                     }
                     //次要移动
                     if (empty($noteData)) {
@@ -91,7 +92,7 @@ class PlayerController extends Controller
                     if (empty($noteData)) {
                         $noteData = (new TankModel())->computeRandom(array_merge($tankData, $newItem), $newRowKey, $newColKey, $newMap);
                     }
-                    $useGlod = (new TankModel())->computeGlod(array_merge($tankData, $newItem));
+                    $useGlod        = (new TankModel())->computeGlod(array_merge($tankData, $newItem));
                     $responseData[] = [
                         'tId'       => $tankData['tId'],
                         'direction' => $noteData['direction'],
@@ -102,8 +103,8 @@ class PlayerController extends Controller
             }
         }
         //判断是否有坦克死亡
-        $tidArr=array_column($responseData,'tId');
-        for($i=1;$i<= 5; $i++) {
+        $tidArr = array_column($responseData, 'tId');
+        for ($i = 1; $i <= 5; $i++) {
             if (!in_array("{$this->teamId}{$i}", $tidArr)) {
                 $responseData[] = [
                     'tId'       => "{$this->teamId}{$i}",
@@ -113,7 +114,7 @@ class PlayerController extends Controller
                     'useGlod'   => true];
             }
         }
-//        Log::info($responseData);
+        //        Log::info($responseData);
         return $this->apiReturn($responseData, '/action');
     }
 }
--- app/Http/Models/TankModel.php
+++ app/Http/Models/TankModel.php
@@ -25,17 +25,17 @@ class TankModel
         'M6' => -350,
         'M7' => -350,
         'M8' => -350,
-        'A1' => 50,
-        'B1' => 80,
-        'B2' => 20,
-        'B3' => 40,
-        'B4' => 40,
-        'B5' => 60,
-        'C1' => 80,
-        'C2' => 20,
-        'C3' => 40,
-        'C4' => 40,
-        'C5' => 60,];
+        'A1' => 2500,
+        'B1' => 800,
+        'B2' => 200,
+        'B3' => 400,
+        'B4' => 400,
+        'B5' => 600,
+        'C1' => 800,
+        'C2' => 200,
+        'C3' => 400,
+        'C4' => 400,
+        'C5' => 600,];
 
     public static $elementFlog = [
         'M1' => 1,
@@ -58,101 +58,89 @@ class TankModel
         'C4' => 4,
         'C5' => 4,];
 
-    public static $BTanks=[
-        'B1'=>[
-            "name"=> "K2黑豹",
-            "gongji"=> 3,
-            "shengming"=> 5,
-            "shengyushengming"=> 5,
-            "yidong"=> 1,
-            "shecheng"=> 1,
-            "shiye"=> 1
-        ],
-        'B2'=>[
-            "name"=> "T-90",
-            "gongji"=> 1,
-            "shengming"=> 10,
-            "shengyushengming"=> 10,
-            "yidong"=> 1,
-            "shecheng"=> 1,
-            "shiye"=> 1
-        ],
-        'B3'=>[
-            "name"=> "K2黑豹",
-            "gongji"=> 3,
-            "shengming"=> 5,
-            "shengyushengming"=> 5,
-            "yidong"=> 1,
-            "shecheng"=> 1,
-            "shiye"=> 1
-        ],
-        'B4'=>[
-            "name"=> "阿马塔",
-            "gongji"=> 1,
-            "shengming"=> 4,
-            "shengyushengming"=> 4,
-            "yidong"=> 2,
-            "shecheng"=> 1,
-            "shiye"=> 2
-        ],
-        'B5'=>[
-            "name"=> "99主战坦克",
-            "gongji"=> 1,
-            "shengming"=> 3,
-            "shengyushengming"=> 3,
-            "yidong"=> 1,
-            "shecheng"=> 3,
-            "shiye"=> 1
-        ],
-    ];
+    public static $BTanks = [
+        'B1' => [
+            "name"             => "K2黑豹",
+            "gongji"           => 3,
+            "shengming"        => 5,
+            "shengyushengming" => 5,
+            "yidong"           => 1,
+            "shecheng"         => 1,
+            "shiye"            => 1],
+        'B2' => [
+            "name"             => "T-90",
+            "gongji"           => 1,
+            "shengming"        => 10,
+            "shengyushengming" => 10,
+            "yidong"           => 1,
+            "shecheng"         => 1,
+            "shiye"            => 1],
+        'B3' => [
+            "name"             => "K2黑豹",
+            "gongji"           => 3,
+            "shengming"        => 5,
+            "shengyushengming" => 5,
+            "yidong"           => 1,
+            "shecheng"         => 1,
+            "shiye"            => 1],
+        'B4' => [
+            "name"             => "阿马塔",
+            "gongji"           => 1,
+            "shengming"        => 4,
+            "shengyushengming" => 4,
+            "yidong"           => 2,
+            "shecheng"         => 1,
+            "shiye"            => 2],
+        'B5' => [
+            "name"             => "99主战坦克",
+            "gongji"           => 1,
+            "shengming"        => 3,
+            "shengyushengming" => 3,
+            "yidong"           => 1,
+            "shecheng"         => 3,
+            "shiye"            => 1],];
 
-    public static $CTanks=[
-        'C1'=>[
-            "name"=> "K2黑豹",
-            "gongji"=> 3,
-            "shengming"=> 5,
-            "shengyushengming"=> 5,
-            "yidong"=> 1,
-            "shecheng"=> 1,
-            "shiye"=> 1
-        ],
-        'C2'=>[
-            "name"=> "T-90",
-            "gongji"=> 1,
-            "shengming"=> 10,
-            "shengyushengming"=> 10,
-            "yidong"=> 1,
-            "shecheng"=> 1,
-            "shiye"=> 1
-        ],
-        'C3'=>[
-            "name"=> "K2黑豹",
-            "gongji"=> 3,
-            "shengming"=> 5,
-            "shengyushengming"=> 5,
-            "yidong"=> 1,
-            "shecheng"=> 1,
-            "shiye"=> 1
-        ],
-        'C4'=>[
-            "name"=> "阿马塔",
-            "gongji"=> 1,
-            "shengming"=> 4,
-            "shengyushengming"=> 4,
-            "yidong"=> 2,
-            "shecheng"=> 1,
-            "shiye"=> 2
-        ],
-        'C5'=>[
-            "name"=> "99主战坦克",
-            "gongji"=> 1,
-            "shengming"=> 3,
-            "shengyushengming"=> 3,
-            "yidong"=> 1,
-            "shecheng"=> 3,
-            "shiye"=> 1
-        ],
-    ];
+    public static $CTanks = [
+        'C1' => [
+            "name"             => "K2黑豹",
+            "gongji"           => 3,
+            "shengming"        => 5,
+            "shengyushengming" => 5,
+            "yidong"           => 1,
+            "shecheng"         => 1,
+            "shiye"            => 1],
+        'C2' => [
+            "name"             => "T-90",
+            "gongji"           => 1,
+            "shengming"        => 10,
+            "shengyushengming" => 10,
+            "yidong"           => 1,
+            "shecheng"         => 1,
+            "shiye"            => 1],
+        'C3' => [
+            "name"             => "K2黑豹",
+            "gongji"           => 3,
+            "shengming"        => 5,
+            "shengyushengming" => 5,
+            "yidong"           => 1,
+            "shecheng"         => 1,
+            "shiye"            => 1],
+        'C4' => [
+            "name"             => "阿马塔",
+            "gongji"           => 1,
+            "shengming"        => 4,
+            "shengyushengming" => 4,
+            "yidong"           => 2,
+            "shecheng"         => 1,
+            "shiye"            => 2],
+        'C5' => [
+            "name"             => "99主战坦克",
+            "gongji"           => 1,
+            "shengming"        => 3,
+            "shengyushengming" => 3,
+            "yidong"           => 1,
+            "shecheng"         => 3,
+            "shiye"            => 1],];
 
     public function computeAttack($tank, $row, $col, $map)
     {
@@ -178,7 +166,7 @@ class TankModel
                     }
                     $noteArea = $map[$elementRow][$col];
                     //判断是否有物体能被射击
-                    $canAttack=$this->canAttack($tank,$noteArea,$enemyTeamId,$i);
+                    $canAttack = $this->canAttack($tank, $noteArea, $enemyTeamId, $i);
                     if (!$canAttack) {
                         continue;
                     }
@@ -200,7 +188,7 @@ class TankModel
                     }
                     $noteArea = $map[$row][$elementCol];
                     //判断是否有物体能被射击
-                    $canAttack=$this->canAttack($tank,$noteArea,$enemyTeamId,$i);
+                    $canAttack = $this->canAttack($tank, $noteArea, $enemyTeamId, $i);
                     if (!$canAttack) {
                         continue;
                     }
@@ -221,7 +209,7 @@ class TankModel
                     }
                     $noteArea = $map[$elementRow][$col];
                     //判断是否有物体能被射击
-                    $canAttack=$this->canAttack($tank,$noteArea,$enemyTeamId,$i);
+                    $canAttack = $this->canAttack($tank, $noteArea, $enemyTeamId, $i);
                     if (!$canAttack) {
                         continue;
                     }
@@ -242,7 +230,7 @@ class TankModel
                     }
                     $noteArea = $map[$row][$elementCol];
                     //判断是否有物体能被射击
-                    $canAttack=$this->canAttack($tank,$noteArea,$enemyTeamId,$i);
+                    $canAttack = $this->canAttack($tank, $noteArea, $enemyTeamId, $i);
                     if (!$canAttack) {
                         continue;
                     }
@@ -264,31 +252,35 @@ class TankModel
         return $areaWeightsMax;
     }
 
-    public function canAttack($tank,$noteArea,$enemyTeamId,$shecheng){
-        $boos = (new PlayerController())->boos;
-        $teamId=(new PlayerController())->teamId;
+    public function canAttack($tank, $noteArea, $enemyTeamId, $shecheng)
+    {
+        $canAttack=false;
+        $boos   = (new PlayerController())->boos;
+        $teamId = (new PlayerController())->teamId;
+        $team   = (new PlayerController())->team;
+        $gold   = (new PlayerController())->request[$team]['glod'];
         if ($noteArea['element'] === 'A1' && $boos['tanks'][0]['shengyushengming'] <= $tank['gongji']) {
             $canAttack = true;
-        } else {
-            $canAttack = preg_match("/{$enemyTeamId}\d/", $noteArea['element']);
-            if($canAttack){
-                $keyTanks="{$enemyTeamId}Tanks";
-                $tanksData=self::$$keyTanks;
-                //攻击小于敌方不打
-                if($tank['gongji']<$tanksData[$noteArea['element']]['gongji']){
-                    $canAttack=false;
-                }
-                //如果敌方是守 那只让我方守打
-                if(preg_match("/{$enemyTeamId}2/", $noteArea['element'])){
-                    if($tank['tId']!=="{$teamId}2"){
-                        $canAttack=false;
-                    }
-                }
-                //我方远射程 与敌方坦克距离小于等于1
-                if($tank['tId']==="{$teamId}5" && $shecheng<=1){
-                    $canAttack=false;
-                }
-            }
+        } else if (preg_match("/{$enemyTeamId}\d/", $noteArea['element'])) {
+            $canAttack = true;
+            $keyTanks  = "{$enemyTeamId}Tanks";
+            $tanksData = self::$$keyTanks;
+            //攻击小于敌方不打
+            //                if($tank['gongji']<$tanksData[$noteArea['element']]['gongji']){
+            //                    $canAttack=false;
+            //                }
+            //                //如果敌方是守 那只让我方守打
+            //                if($gold==0 && preg_match("/{$enemyTeamId}2/", $noteArea['element'])){
+            //                    if($tank['tId']!=="{$teamId}2"){
+            //                        $canAttack=false;
+            //                    }
+            //                }
+            //                //我方远射程 与敌方坦克距离小于等于1
+            //                if($tank['tId']==="{$teamId}5" && $shecheng<=1){
+            //                    $canAttack=false;
+            //                }
+        }elseif($noteArea['element'] === 'A1'){
+            $canAttack=true;
         }
         return $canAttack;
     }
@@ -633,7 +625,7 @@ class TankModel
             for ($i = $row; $i >= 0; $i--) {
                 for ($k = 0; $k < count($map[0], 0); $k++)
                     if (preg_match("/{$teamId}\d{1}/", $map[$i][$k]['element'])) {
-                        //                    $weights -= 30;
+                                            $weights += 30;
                     } else if (preg_match("/M[4-8]{1}/", $map[$i][$k]['element'])) {
 
                     } else {
@@ -644,7 +636,7 @@ class TankModel
             for ($i = $col; $i < count($map[0], 0); $i++) {
                 for ($k = 0; $k < count($map, 0); $k++)
                     if (preg_match("/{$teamId}\d{1}/", $map[$k][$i]['element'])) {
-                        //                    $weights -= 30;
+                        $weights += 30;
                     } else if (preg_match("/M[4-8]{1}/", $map[$k][$i]['element'])) {
 
                     } else {
@@ -655,7 +647,7 @@ class TankModel
             for ($i = $row; $i < count($map, 0); $i++) {
                 for ($k = 0; $k < count($map[0], 0); $k++)
                     if (preg_match("/{$teamId}\d{1}/", $map[$i][$k]['element'])) {
-                        //                    $weights -= 30;
+                        $weights += 30;
                     } else if (preg_match("/M[4-8]{1}/", $map[$i][$k]['element'])) {
 
                     } else {
@@ -666,7 +658,7 @@ class TankModel
             for ($i = $col; $i >= 0; $i--) {
                 for ($k = 0; $k < count($map, 0); $k++)
                     if (preg_match("/{$teamId}\d{1}/", $map[$k][$i]['element'])) {
-                        //                    $weights -= 30;
+                        $weights += 30;
                     } else if (preg_match("/M[4-8]{1}/", $map[$k][$i]['element'])) {
 
                     } else {
